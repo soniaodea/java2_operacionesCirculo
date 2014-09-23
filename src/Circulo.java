0ff8c54b4fc94bff518534763dcebda6ca67f917
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-09-23 09:46:35 +0200

    Circle operations v1.0

---
+++ src/Circulo.java
@@ -0,0 +1,26 @@
+public class Circulo {
+
+	static final double NUM_PI = 3.1416;
+
+	double radio;
+
+	//Método que dado el radio calcula la circunferencia de un círculo.
+	public double circunferencia(double radio) {
+
+		double result;
+
+		result = 2*NUM_PI*radio;
+
+		return result;
+	}
+
+	//Método que dado el radio calcula el area de un círculo.
+	public double area(double radio) {
+
+		double result;
+
+		result = NUM_PI*radio*radio;
+
+		return result;
+	}
+}
---
+++ src/OperacionesCirculo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OperacionesCirculo {
+
+	public static void main (String[] args) {
+	
+	  double radio;
+          double result;
+	  Circulo circulo = new Circulo();
+
+	  Scanner sc = new Scanner(System.in);
+          System.out.print("\ningresa el radio: ");
+          radio = sc.nextDouble();
+
+	
+	  result =  circulo.circunferencia(radio);
+	  System.out.println("La circunferencia es: " + result); 
+			
+	  result =  circulo.area(radio);
+	  System.out.println("El area es: " + result); 
+	}
+
+}
